d88ff3140d674094e3f9ac7d9a55f3ed4cdc1fd8
Plamen Kovandjiev <p.kovandjiev@kmpelectronics.eu>
2018-01-28 17:29:23 +0200

    Add Tcp temperature example

--- KMPDinoEthernet/src/KMPDinoEthernet/examples/TcpTemperature/TcpTemperature.ino
+++ KMPDinoEthernet/src/KMPDinoEthernet/examples/TcpTemperature/TcpTemperature.ino
@@ -1,35 +1,43 @@
-// TcpRelay.ino
-// Company: KMP Electronics Ltd, Bulgaria.
+// TcpTemperature.ino
+// Company: KMP Electronics Ltd, Bulgaria
 // Web: http://kmpelectronics.eu/
 // License: See the GNU General Public License for more details at http://www.gnu.org/copyleft/gpl.html
-// Supported boards: 
+// Supported boards:
 //		KMP DiNo II NETBOARD V1.0. Web: http://kmpelectronics.eu/en-us/products/dinoii.aspx
 //		ProDiNo NetBoard V2.1. Web: http://kmpelectronics.eu/en-us/products/prodinoethernet.aspx
 // Description:
-//		Tcp server relay manipulation example. 
-// Example link: http://www.kmpelectronics.eu/en-us/examples/dinoii/tcprelaycontrol.aspx
+//		Tcp server read temperature example. 
+// Example link: http://www.kmpelectronics.eu/en-us/examples/dinoii/tcpoptoinputscheck.aspx
 // Version: 1.2.0
-// Date: 29.11.2015
+// Date: 28.01.2018
 // Author: Plamen Kovandjiev <p.kovandiev@kmpelectronics.eu>
-// Description: Compatibilie Arduinio version >= 1.6.5
+// Description: Compatibility Arduino version >= 1.6.5
 
 #include <SPI.h>
 #include <Ethernet.h>
 #include "KmpDinoEthernet.h"
 #include "KMPCommon.h"
 
+// One Wire headers
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
 // If in debug mode - print debug information in Serial. Comment in production code, this bring performance.
 // This method is good for development and verification of results. But increases the amount of code and decreases productivity.
-//#define DEBUG
+#define DEBUG
+
+// Thermometer Resolution in bits. http://datasheets.maximintegrated.com/en/ds/DS18B20.pdf page 8. 
+// Bits - CONVERSION TIME. 9 - 93.75ms, 10 - 187.5ms, 11 - 375ms, 12 - 750ms. 
+#define TEMPERATURE_PRECISION 10
 
 // Enter a MAC address and IP address for your controller below.
-byte _mac[] = { 0x00, 0x08, 0xDC, 0x2B, 0x82, 0xC0 };
+byte _mac[] = { 0x00, 0x08, 0xDC, 0x14, 0x1E, 0xF1 };
 
 // The IP address will be dependent on your local network.
 IPAddress _ip(192, 168, 1, 199);
 
 // Local port.
-const uint16_t LOCAL_PORT = 1111;
+const unsigned int _localPort = 1111;
 
 // Define constants.
 const char PREFIX[] = "FF";
@@ -40,14 +48,18 @@ const uint8_t BUFF_SIZE = 24;
 // Buffer for receiving data.
 char _packetBuffer[BUFF_SIZE];
 
-char _buffer[10];
-
 // Initialize the Ethernet server library
-// with the IP address and port you want to use 
-EthernetServer _server(LOCAL_PORT);
+// with the IP address and port you want to use.
+EthernetServer _server(_localPort);
 
 // Client.
 EthernetClient _client;
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(OneWirePin);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature _sensors(&oneWire);
 
 /**
 * \brief Setup void. Arduino executed first. Initialize DiNo board and prepare Ethernet connection.
@@ -58,10 +70,10 @@ EthernetClient _client;
 void setup()
 {
 #ifdef DEBUG
-    // Open serial communications and wait for port to open:
+    // Open serial communications and wait for port to open.
     Serial.begin(9600);
     while (!Serial) {
-    	; // wait for serial port to connect. Needed for Leonardo only. If need debug setup() void.
+    	; // wait for serial port to connect. Needed for Leonardo only. If need debug setup() method.
     }
 #endif
 
@@ -73,13 +85,20 @@ void setup()
     _server.begin();
 
 #ifdef DEBUG
-    Serial.println("The server is started.");
+	Serial.println("KMP Electronics Ltd. TCP temperature example.");
+	Serial.println("The server is started.");
     Serial.print(Ethernet.localIP());
 	Serial.print(':');
-	Serial.println(LOCAL_PORT);
-	Serial.println(Ethernet.gatewayIP());
+	Serial.println(_localPort);
+    Serial.println(Ethernet.gatewayIP());
     Serial.println(Ethernet.subnetMask());
 #endif
+
+	// Start the One Wire library.
+	_sensors.begin();
+
+	// Set precision.
+	_sensors.setResolution(TEMPERATURE_PRECISION);
 }
 
 /**
@@ -89,7 +108,7 @@ void setup()
 * \return void
 */
 void loop(){
-    // Listen for incoming clients.
+    // listen for incoming clients
     _client = _server.available();
 
     // If not client - exit.
@@ -122,18 +141,16 @@ void loop(){
 }
 
 /**
- * \brief ReadClientRequest void. Read and parse client request.
- *	  First row of client request is similar to:
- *    Prefix Command
- *         | |
- *         FFR0011
- *            ||||
- *            3  6 relay position. First is 1, second - 2...
- *    You can check communication client-server get program Smart Sniffer: http://www.nirsoft.net/utils/smsniff.html
- * 
- * 
- * \return void
- */
+* \brief ReadClientRequest void. Read and parse client request.
+*	  First row of client request is similar to:
+*    Prefix Command
+*         | |
+*         FFT
+*    You can check communication client-server get program Smart Sniffer: http://www.nirsoft.net/utils/smsniff.html
+*
+*
+* \return void
+*/
 void ReadClientRequest()
 {
 #ifdef DEBUG
@@ -149,61 +166,48 @@ void ReadClientRequest()
     {
         requestLen += readSize;
 #ifdef DEBUG
-        Serial.print(_packetBuffer);
+        Serial.println(_packetBuffer);
+        Serial.println(readSize);
 #endif
         // If packet is not valid - read to end.
-        if(requestLen != 7)
+        if(requestLen != 3)
         {
             continue;
         }
-                
+        
         // Check first 2 chars is prefix.
         // If Prefix not equals first 2 chars not checked any more.
         if(strncmp(_packetBuffer, PREFIX, 2) > 0)
         {
-            continue; 
+            continue;
         }
-        
-        // Command R for relays.
-        if(_packetBuffer[2] == CH_R)
+
+        // Command I for opto inputs.
+        if(_packetBuffer[2] == CH_T)
         {
-            _commandSend = CH_R;
-            int statusPos = 3; // To check new statuses.
-            char dataChar;
-            // Read chars for On or Off for all relays.
-            for(int i = 0; i < RELAY_COUNT; i++)
-            {
-                dataChar = _packetBuffer[statusPos];
-                bool status;
-                if(dataChar == CH_0 || dataChar == CH_1)
-                {
-                    // if c == 1 - true. if c == 0 false.
-                    status = dataChar == CH_1;
-                    SetRelayStatus(i, status);
-                }
-                else
-                    break; // Char is not (0, 1) valid.
-                    
-                statusPos++;
-            }
+            _commandSend = CH_T;
         }
     }
 
 #ifdef DEBUG
     Serial.println();
-    if(_commandSend == CH_R)
-        Serial.println("Relay command sended.");
+    if(_commandSend == CH_T)
+    {
+        Serial.println("Check temperature command sent.");
+    }
     else
+    {
         Serial.println("Command is not valid.");
+    }
 #endif
 }
 
 /**
- * \brief WriteClientResponse void. Write html response.
- * 
- * 
- * \return void
- */
+* \brief WriteClientResponse void. Write response.
+*
+*
+* \return void
+*/
 void WriteClientResponse()
 {
 #ifdef DEBUG
@@ -211,26 +215,50 @@ void WriteClientResponse()
 #endif
 
     if (!_client.connected())
-	{
+    {
         return;
-	}
+    }
 
     // Response write.
-    _client.write(PREFIX);
-    if(_commandSend == CH_R)
+    if(_commandSend == CH_T)
     {
-        _client.write(CH_R);
-        // Add relay data
-        for (int i = 0; i < RELAY_COUNT; i++)
-        {
+		// Send the command to get temperatures.
+		_sensors.requestTemperatures();
+
+		// Grab a count of devices on the wire.
+		int numberOfOneWireDevices = _sensors.getDeviceCount();
 #ifdef DEBUG
-            Serial.print("Relay ");
-			IntToChars(i + 1, _buffer);
-			Serial.print(_buffer);
-            Serial.print(" ");
-            Serial.println(GetRelayStatus(i) ? W_ON : W_OFF);
+		Serial.print("Number of One Wire devices: ");
+		Serial.println(numberOfOneWireDevices);
 #endif
-            _client.write(GetRelayStatus(i) ? CH_1 : CH_0);
+		// Temp device address.
+		DeviceAddress tempDeviceAddress;
+		char buffer[15];
+
+		// Add temperature data
+        for (int i = 0; i < numberOfOneWireDevices; i++)
+        {
+			if (i > 0)
+			{
+				_client.write(CR_LF);
+			}
+
+			float temp;
+			bool sensorAvaible = _sensors.getAddress(tempDeviceAddress, i);
+			if (sensorAvaible)
+			{
+				_client.write(PREFIX);
+				_client.write(CH_T);
+				temp = _sensors.getTempC(tempDeviceAddress);
+#ifdef DEBUG
+				Serial.print("Temperature ");
+				Serial.print(i);
+				Serial.print(" sensor: ");
+				Serial.println(temp);
+#endif
+				FloatToChars(temp, 1, buffer);
+				_client.write(buffer);
+			}
         }
     }
 }
