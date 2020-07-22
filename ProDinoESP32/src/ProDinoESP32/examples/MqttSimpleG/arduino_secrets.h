65168b327145aa3226afc532aca138c1bad6ad44
Plamen Kovandjiev <p.kovandjiev@kmpelectronics.eu>
2020-07-22 21:02:38 +0300

    Adding first MQTT example

---
+++ ProDinoESP32/src/ProDinoESP32/examples/MqttSimpleG/arduino_secrets.h
@@ -0,0 +1,11 @@
+// SIM card pin number if exists
+#define SECRET_PINNUMBER     ""
+#define SECRET_GPRS_APN      "" // replace your GPRS APN
+#define SECRET_GPRS_LOGIN    "" // replace with your GPRS login
+#define SECRET_GPRS_PASSWORD "" // replace with your GPRS password
+
+// MQTT server settings. 
+#define MQTT_SERVER           "" 	// Change it with yours data.
+#define MQTT_PORT             12345 // Change it with yours data.
+#define MQTT_USER             "" 	// Change it with yours data.
+#define MQTT_PASS             "" 	// Change it with yours data.
